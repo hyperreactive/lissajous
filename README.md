f75ee9cae71b3b0a6bd86bc150015a9f09ba0792
Morten Barklund <morten@barklund.dk>
2018-09-30 18:16:42 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
+
+## Lissajous Part 1
+
+Please see the [accompanying article](https://medium.com/hyperreactive/unit-testing-react-part-1-setting-up-c6a24b3babcc).
---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <main className="lissajous">
+        <h1 className="lissajous-title">Welcome to Lissajous</h1>
+      </main>
+    );
+  }
+}
+
+export default App;
